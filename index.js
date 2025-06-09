2088b2a991d1d6d199222718d35d10cfc6af89fc
Nguyen Thien Hung <ngthienhung277@users.noreply.github.com>
2025-06-09 17:36:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const request = require("request-promise");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// const apiKey = "c1caeda9d2f668bf620ba03764950b49";
+
+const generateScraperUrl = (apiKey) => `https://api.scraperapi.com/?api_key=${apiKey}&autoparse=true`;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Amazon Scrapper API.");
+});
+
+//get product details
+app.get("/products/:productId", async (req, res) => {
+    const { productId } = req.params;
+    const { apiKey } = req.query;
+    try {
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/dp/${productId}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+//get product reviews
+app.get("/products/:productId/reviews", async (req, res) => {
+    const { productId } = req.params;
+    const { apiKey } = req.query;
+    try {
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/product-reviews/${productId}&page=${page}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+//get product offers
+app.get("/products/:productId/offers", async (req, res) => {
+    const { productId } = req.params;
+    const { apiKey } = req.query;
+
+    try {
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+//get search results
+app.get("/search/:searchQuery", async (req, res) => {
+    const { searchQuery } = req.params;
+    const { apiKey } = req.query;
+    try {
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/s?k=${searchQuery}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+//get deals
+app.get("/deals", async (req, res) => {
+    const { apiKey } = req.query;
+    
+    try {
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/gp/goldbox?ref_=nav_cs_gb`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json({ error: error.message });
+    }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
