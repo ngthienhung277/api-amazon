eace5a858740aa367f166a7df37a3b076214bb94
Nguyen Thien Hung <ngthienhung277@users.noreply.github.com>
2025-06-10 10:27:24 +0700

    final

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const request = require("request-promise");
 
-const PORT = process.env.PORT || 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
